a0e2fbea932b30a1731f45cfbd6bc7f23d1f5746
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-08-27 16:58:49 +0500

    t2

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
     
 $host='localhost';
 $username='root';
-$user_pass='pass';
+$user_pass='rqkhbu6t';
 $database_in_use='rinder';
 
 $con = mysqli_connect($host,$username,$user_pass,$database_in_use);
